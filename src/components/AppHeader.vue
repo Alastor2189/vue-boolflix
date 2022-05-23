4ea61d3515423627c2f597c874b767122fd3db90
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-23 22:41:41 +0200

    creazione header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+      <div class="title">
+          <h2>
+              Boolflix
+          </h2>
+      </div>
+      <div class="search">
+          <input type="text" placeholder="Cerca un film/serie Tv" v-model="searchVideo">
+          <button @click="$emit('filmResearch', 'research')">
+            Invia
+          </button>
+      </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            searchVideo:"",
+        }
+    },
+    methods: {
+        searchList () {
+
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
