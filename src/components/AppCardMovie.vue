667c5b83bbf86eb0aaca0b380439d98a45f0a9e7
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-25 16:39:07 +0200

    inserimento bandiere

--- src/components/AppCardMovie.vue
+++ src/components/AppCardMovie.vue
@@ -3,8 +3,40 @@
         <div class="card">
           <h2> {{ video.title }}</h2>
           <h4> {{ video.original_title  }}</h4> 
-          <p> {{ video.original_language  }}</p>
+        <div class="flag">
+            <div v-if="video.original_language === 'it'">
+            <h4>Lingua:</h4>
+            <span :class="`fi fi-it`"></span>
+            </div>
+            <div v-else-if="video.original_language === 'es'">
+            <h4>Lingua:</h4>
+            <span :class="`fi fi-es`"></span>
+            </div>
+            <div v-else-if="video.original_language === 'en'">
+            <h4>Lingua:</h4>
+            <span :class="`fi fi-gb`"></span>
+            </div>
+            <div v-else-if="video.original_language === 'ja'">
+            <h4>Lingua:</h4>
+            <span :class="`fi fi-jp`"></span>
+            </div>
+            <div v-else-if="video.original_language === 'fr'">
+            <h4>Lingua:</h4>
+            <span :class="`fi fi-fr`"></span>
+            </div>
+            <div v-else>
+            <h4>Lingua:</h4>
+            </div>
+            <p> {{ video.original_language  }}</p>
+        </div>
+          
           <p> {{ video.vote_average }}</p>
+        </div>
+        <div class="image">
+            <img
+          :src="`https://image.tmdb.org/t/p/w342/${video.poster_path}`"
+          :alt="video.title"
+        />
         </div>
     </li>
   
@@ -20,6 +52,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~flag-icons/css/flag-icons.css";
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
     .menu-card {
         width: calc(100% / 4 - 8px);
         margin: 4px;
